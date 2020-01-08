2203ea2bf5e994b45eeb12c5471515c8cf1e5812
Miguel del Mazo <migueldelmazo@gmail.com>
2020-01-08 11:05:04 +0100

    Added clousure example

---
+++ js-clousure/README.md
@@ -0,0 +1,8 @@
+# Explicación de Clousure
+
+La clousure es una herramienta muy práctica de JS, que además suelen preguntar en las entrevistas de trabajo.
+
+Os recomendamos dos cosas:
+
+- Ver el [vídeo de esta carpeta](./info/clousure.mp4).
+- Antes de ir a una entrevista de trabajo buscar en google la definición de clousure en JS, ya que es fácil de entender y aplicar pero es difícil de explicar.
---
+++ js-clousure/js/main.js
@@ -0,0 +1,14 @@
+const one = 1;
+
+const addEvent = (inputSelector, resultSelector, prefix = '') => {
+  const inputHandle = (ev) => {
+    debugger;
+    const resultElement = document.querySelector(resultSelector);
+    resultElement.innerHTML = prefix + ev.target.value;
+  }
+  const inputElement = document.querySelector(inputSelector);
+  inputElement.addEventListener('keyup', inputHandle);
+}
+
+addEvent('.js-name', '.js-result-name', '');
+addEvent('.js-github', '.js-result-github', 'https://github.com/');
